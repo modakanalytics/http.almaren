233564b52c773d8b6db3887ba3bc62c63aec18d7
badrinath.patchikolla <badrinath.patchikolla@modakanalytics.com>
2021-04-16 16:54:32 +0530

    fixed bug for params fields

--- src/main/scala/com/github/music/of/the/ainur/almaren/http/HTTPConnector.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/http/HTTPConnector.scala
@@ -13,14 +13,14 @@ import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Try}
 
 private[almaren] final case class Result(
-  `__ID__`:String,
-  `__BODY__`:Option[String] = None,
-  `__HEADER__`:Map[String,Seq[String]] = Map(),
-  `__STATUS_CODE__`:Option[Int] = None,
-  `__STATUS_MSG__`:Option[String] = None,
-  `__ERROR__`:Option[String] = None,
-  `__ELAPSED_TIME__`:Long
-)
+                                          `__ID__`: String,
+                                          `__BODY__`: Option[String] = None,
+                                          `__HEADER__`: Map[String, Seq[String]] = Map(),
+                                          `__STATUS_CODE__`: Option[Int] = None,
+                                          `__STATUS_MSG__`: Option[String] = None,
+                                          `__ERROR__`: Option[String] = None,
+                                          `__ELAPSED_TIME__`: Long
+                                        )
 
 object Alias {
   val DataCol = "__DATA__"
@@ -30,37 +30,38 @@ object Alias {
 
 
 private[almaren] case class MainHTTP(
-  headers:Map[String,String],
-  method:String,
-  requestHandler:(Row,Session,String,Map[String,String],String,Int,Int) => requests.Response,
-  session:() => requests.Session,
-  connectTimeout:Int,
-  readTimeout:Int,
-  threadPoolSize:Int,
-  batchSize:Int) extends Main {
+                                      headers: Map[String, String],
+                                      params: Map[String, String],
+                                      method: String,
+                                      requestHandler: (Row, Session, String, Map[String, String], Map[String, String], String, Int, Int) => requests.Response,
+                                      session: () => requests.Session,
+                                      connectTimeout: Int,
+                                      readTimeout: Int,
+                                      threadPoolSize: Int,
+                                      batchSize: Int) extends Main {
 
   override def core(df: DataFrame): DataFrame = {
-    logger.info(s"headers:{$headers}, method:{$method}, connectTimeout:{$connectTimeout}, readTimeout{$readTimeout}, threadPoolSize:{$threadPoolSize}")
-    
+    logger.info(s"headers:{$headers},params:{$params}, method:{$method}, connectTimeout:{$connectTimeout}, readTimeout{$readTimeout}, threadPoolSize:{$threadPoolSize}")
+
     import df.sparkSession.implicits._
 
     val result = df.mapPartitions(partition => {
 
-      implicit val ec:ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(threadPoolSize))
-      val data:Iterator[Future[Seq[Result]]] = partition.grouped(batchSize).map(rows => Future {
+      implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(threadPoolSize))
+      val data: Iterator[Future[Seq[Result]]] = partition.grouped(batchSize).map(rows => Future {
         val s = session()
-        rows.map(row => request(row,s))
+        rows.map(row => request(row, s))
       })
-      val requests:Future[Iterator[Seq[Result]]] = Future.sequence(data)
-      Await.result(requests,Duration.Inf).flatMap(s => s)
+      val requests: Future[Iterator[Seq[Result]]] = Future.sequence(data)
+      Await.result(requests, Duration.Inf).flatMap(s => s)
     })
     result.toDF
   }
 
-  private def request(row:Row,session:Session): Result = {
+  private def request(row: Row, session: Session): Result = {
     val url = row.getAs[Any](Alias.UrlCol).toString()
     val startTime = System.currentTimeMillis()
-    val response = Try(requestHandler(row,session,url,headers,method,connectTimeout,readTimeout))
+    val response = Try(requestHandler(row, session, url, headers, method, connectTimeout, readTimeout))
     val elapsedTime = System.currentTimeMillis() - startTime
     val id = row.getAs[Any](Alias.IdCol).toString()
     response match {
@@ -72,8 +73,8 @@ private[almaren] case class MainHTTP(
         Some(r.statusMessage),
         `__ELAPSED_TIME__` = elapsedTime)
       case Failure(f) => {
-            logger.error("Almaren HTTP Request Error",f)
-              Result(id,`__ERROR__` = Some(f.getMessage()), `__ELAPSED_TIME__` = elapsedTime)
+        logger.error("Almaren HTTP Request Error", f)
+        Result(id, `__ERROR__` = Some(f.getMessage()), `__ELAPSED_TIME__` = elapsedTime)
       }
     }
   }
@@ -81,17 +82,19 @@ private[almaren] case class MainHTTP(
 
 private[almaren] trait HTTPConnector extends Core {
 
-  def http( 
-    headers:Map[String,String] = Map(),
-    method:String,
-    requestHandler:(Row,Session,String,Map[String,String],String,Int,Int) => requests.Response = HTTP.defaultHandler,
-    session:() => requests.Session = HTTP.defaultSession,
-    connectTimeout:Int = 60000,
-    readTimeout:Int = 1000,
-    threadPoolSize:Int = 1,
-    batchSize:Int = 5000): Option[Tree] =
+  def http(
+            headers: Map[String, String] = Map(),
+            params: Map[String, String] = Map(),
+            method: String,
+            requestHandler: (Row, Session, String, Map[String, String], Map[String, String], String, Int, Int) => requests.Response = HTTP.defaultHandler,
+            session: () => requests.Session = HTTP.defaultSession,
+            connectTimeout: Int = 60000,
+            readTimeout: Int = 1000,
+            threadPoolSize: Int = 1,
+            batchSize: Int = 5000): Option[Tree] =
     MainHTTP(
       headers,
+      params,
       method,
       requestHandler,
       session,
@@ -100,18 +103,18 @@ private[almaren] trait HTTPConnector extends Core {
       threadPoolSize,
       batchSize
     )
-  
+
 }
 
 object HTTP {
-  val defaultHandler = (row:Row,session:Session,url:String, headers:Map[String,String], method:String,connectTimeout:Int, readTimeout:Int) => {
+  val defaultHandler = (row: Row, session: Session, url: String, headers: Map[String, String], params: Map[String, String], method: String, connectTimeout: Int, readTimeout: Int) => {
     method.toUpperCase match {
-      case "GET" => session.get(url, params = headers, readTimeout = readTimeout, connectTimeout = connectTimeout)
-      case "DELETE" => session.delete(url, params = headers, readTimeout = readTimeout, connectTimeout = connectTimeout)
-      case "OPTIONS" => session.options(url, params = headers, readTimeout = readTimeout, connectTimeout = connectTimeout)
-      case "HEAD" => session.head(url, params = headers, readTimeout = readTimeout, connectTimeout = connectTimeout)
-      case "POST" => session.post(url, params = headers, data = row.getAs[String](Alias.DataCol), readTimeout = readTimeout, connectTimeout = connectTimeout)
-      case "PUT" => session.put(url, params = headers, data = row.getAs[String](Alias.DataCol), readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "GET" => session.get(url, headers = headers, params = params, readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "DELETE" => session.delete(url, headers = headers, params = params, readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "OPTIONS" => session.options(url, headers = headers, params = params, readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "HEAD" => session.head(url, headers = headers, params = params, readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "POST" => session.post(url, headers = headers, params = params, data = row.getAs[String](Alias.DataCol), readTimeout = readTimeout, connectTimeout = connectTimeout)
+      case "PUT" => session.put(url, headers = headers, params = params, data = row.getAs[String](Alias.DataCol), readTimeout = readTimeout, connectTimeout = connectTimeout)
       case method => throw new Exception(s"Invalid Method: $method")
     }
   }
@@ -119,4 +122,5 @@ object HTTP {
   val defaultSession = () => requests.Session()
 
   implicit class HTTPImplicit(val container: Option[Tree]) extends HTTPConnector
+
 }
