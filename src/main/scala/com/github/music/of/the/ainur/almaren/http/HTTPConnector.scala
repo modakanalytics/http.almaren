642ec7591a99bb3ee4840e26678a8fac947c3e83
badrinath.patchikolla <badrinath.patchikolla@modakanalytics.com>
2021-04-16 17:11:01 +0530

    fixed bug for params fields

--- src/main/scala/com/github/music/of/the/ainur/almaren/http/HTTPConnector.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/http/HTTPConnector.scala
@@ -13,13 +13,13 @@ import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Try}
 
 private[almaren] final case class Result(
-  `__ID__`: String,
-  `__BODY__`: Option[String] = None,
-  `__HEADER__`: Map[String, Seq[String]] = Map(),
-  `__STATUS_CODE__`: Option[Int] = None,
-  `__STATUS_MSG__`: Option[String] = None,
-  `__ERROR__`: Option[String] = None,
-  `__ELAPSED_TIME__`: Long
+  `__ID__`:String,
+  `__BODY__`:Option[String] = None,
+  `__HEADER__`:Map[String, Seq[String]] = Map(),
+  `__STATUS_CODE__`:Option[Int] = None,
+  `__STATUS_MSG__`:Option[String] = None,
+  `__ERROR__`:Option[String] = None,
+  `__ELAPSED_TIME__`:Long
                                         )
 
 object Alias {
